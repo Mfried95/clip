116bbd78184b5e9092747ab8be988d62920735ea
Mfried95 <81455905+Mfried95@users.noreply.github.com>
2022-07-15 16:44:26 -0400

    Updates

---
+++ precache-manifest.b1f3dbbf4d9939fab1677a15c3514ce5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ecbdd94e6b30298ea1fe",
+    "url": "/clip/static/css/main.1bcb52c0.chunk.css"
+  },
+  {
+    "revision": "ecbdd94e6b30298ea1fe",
+    "url": "/clip/static/js/main.ecbdd94e.chunk.js"
+  },
+  {
+    "revision": "16d3813476f7dfa3da86",
+    "url": "/clip/static/js/1.16d38134.chunk.js"
+  },
+  {
+    "revision": "90f1a1d5a33259741ec9",
+    "url": "/clip/static/js/runtime~main.90f1a1d5.js"
+  },
+  {
+    "revision": "28caa0248db688b9dfaa91155414ee80",
+    "url": "/clip/static/media/logo.28caa024.svg"
+  },
+  {
+    "revision": "a030348e47bd6610cc92ab378a8d6059",
+    "url": "/clip/static/media/image-computer.a030348e.png"
+  },
+  {
+    "revision": "806276512d2662e155c44b2cc5c98bc1",
+    "url": "/clip/static/media/bg-header-mobile.80627651.png"
+  },
+  {
+    "revision": "df6601b0961197fd570b44815ac44c68",
+    "url": "/clip/static/media/bg-header-desktop.df6601b0.png"
+  },
+  {
+    "revision": "33b174762d64bacee49261a33f5eb147",
+    "url": "/clip/index.html"
+  }
+];
